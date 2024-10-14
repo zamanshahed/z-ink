79a8184ad6bb9cf2cc93cb8e77b9ee86fd003849
sumonshd <sumonshd@gmail.com>
2024-10-14 15:11:16 +0000

    WIP: favicon setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div className={`bg-cMainBgBlack text-cTextPrimary min-h-screen w-full`}>
-      Home
+      Z-Ink
     </div>
   );
 }
