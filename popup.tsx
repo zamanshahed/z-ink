29481d74adcbbcd7e212e647edb4ff1f56f1ab53
genieShahed <genie.shahed@gmail.com>
2024-11-02 23:19:03 +0600

    Base setup with FCP

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,13 @@
 import { useState } from "react"
 
+import "./style.css"
+
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="bg-[#1E1E26] text-[#77A6A3] p-3 w-[400px] h-[250px]">
+      <div className="text-2xl font-bold">Binary in my blood!</div>
     </div>
   )
 }
